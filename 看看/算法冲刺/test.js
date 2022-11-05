c234707d30154a6377cdb84e27dd494a66ae9c05
Xinxin-fire <13166835035@163.com>
2022-11-05 12:42:15 +0800

    算法

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -24,5 +24,35 @@ var sortArray = function(nums) {
   return nums;
 };
 
+// 插入排序
+// 保存当前正在遍历的值，并在已排序好的数组中找到其对应位置
+var sortArray1 = function(nums) {
+  for(let i = 0; i < nums.length; i++) {
+    let j = i;
+    let temp = nums[i];
+    while(j > 0 && nums[j-1] > temp) {
+      [nums[j-1], nums[j]] = [nums[j], nums[j-1]];
+      j--
+    }
+  }
+  return nums;
+};
+console.log(sortArray1([5,2,3,1,4,7]));
+// 归并排序
+// 将数组分成一个个小数组,每个小数组再进行有序合并
 
-console.log(sortArray([5,2,3,1]));
+var sortArray = function(nums) {
+  if (nums.length <= 1) {
+    return nums;
+  }
+  function mergeArr(nums, left, right) {
+    if (right - left < 1) {
+      return nums;
+    }
+    let mid = Math.floor(nums.length / 2);
+    let leftArr =  mergeArr(nums.slice(left, mid));
+    let rightArr =  mergeArr(nums.slice(mid+1, right));
+    return leftArr.concat(rightArr);
+  }
+  mergeArr(nums, 0, nums.length)
+};
