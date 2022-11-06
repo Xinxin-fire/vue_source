6ab6f5c8124b00f5992360ebc9a72fe4184664a7
Xinxin-fire <13166835035@163.com>
2022-11-06 21:38:33 +0800

    算法总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,58 +1,22 @@
-// 排序算法
-// 1.冒泡排序， 每一轮都比较相邻两项，如果第一项比第二项大则交换，
-// 每一层循环都会有一个最大的项被排序出来
-var sortArray = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - 1 - i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        [nums[j], nums[j+ 1]] = [nums[j + 1], nums[j]]
-      }
+var longestPalindrome = function(s) {
+    let length = s.length
+    let dp = new Array(length).fill(0).map(() => new Array(length).fill(0));
+    let res = '';
+    for  (let i = 0; i< length; i++) {
+        dp[i][i] = 1;
     }
-  }
-  return nums;
-};
-// 选择排序
-// 循环遍历数组，每次都找出当前范围内的最小值,把它放在当前范围的头部
-var sortArray = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i+1; j < nums.length; j++) {
-      if (nums[i] > nums[j]) {
-        [nums[j], nums[i]] = [nums[i], nums[j]]
-      }
+    for  (let i = 1; i< length; i++) {
+        for  (let j = i + 1; j < length; j++) {
+            if (s[i] === s[j]) {
+                dp[i][j] = dp[i-1][j-1]
+                if (dp[i][j] && j - i > res.length)  {
+                    res = s.slice(i, j+ 1)
+                }
+            } else {
+                 dp[i][j] = 0
+            }
+        }
     }
-  }
-  return nums;
+    return res;
 };
-
-// 插入排序
-// 保存当前正在遍历的值，并在已排序好的数组中找到其对应位置
-var sortArray1 = function(nums) {
-  for(let i = 0; i < nums.length; i++) {
-    let j = i;
-    let temp = nums[i];
-    while(j > 0 && nums[j-1] > temp) {
-      [nums[j-1], nums[j]] = [nums[j], nums[j-1]];
-      j--
-    }
-  }
-  return nums;
-};
-console.log(sortArray1([5,2,3,1,4,7]));
-// 归并排序
-// 将数组分成一个个小数组,每个小数组再进行有序合并
-
-var sortArray = function(nums) {
-  if (nums.length <= 1) {
-    return nums;
-  }
-  function mergeArr(nums, left, right) {
-    if (right - left < 1) {
-      return nums;
-    }
-    let mid = Math.floor(nums.length / 2);
-    let leftArr =  mergeArr(nums.slice(left, mid));
-    let rightArr =  mergeArr(nums.slice(mid+1, right));
-    return leftArr.concat(rightArr);
-  }
-  mergeArr(nums, 0, nums.length)
-};
+console.log(longestPalindrome("babad"));
--- 看看/算法冲刺/排序算法.js/快速排序.js
+++ 看看/算法冲刺/排序算法.js/快速排序.js
@@ -1,4 +1,5 @@
 // 通过选取数组的中间节点,将大于节点的数排在右边小于节点的数排在左边,这样不断重复上述操作直至排序完成
+// 归并排序递归取中间索引是直接通过二分来取的，而快排是通过函数的左节点的值的索引位置决定的
 function quickSort(arr, left=0, right=arr.length - 1) {
   if (arr.length > 1) {
     let lineIndex = partition(arr, left, right)
@@ -16,12 +17,15 @@ function partition(arr, left, right) {
   let j = right;
   let pivotValue = arr[Math.floor((left + right)/2)]
   while(i <= j) {
+    // 左侧小于中间值，则不用动，将i值向前移动
     while(arr[i] < pivotValue) {
       i++;
     }
+    // 右侧大于中间值，则不用动，将j值向后移动
     while(arr[j] > pivotValue) {
       j--;
     }
+    // 循环到这说明当前i,j的值都不符合其区间，需要交换
     if (i <= j) {
       [arr[i], arr[j]] = [arr[j], arr[i]]
       i++;
