50d7149abf14631db24f6fde5cbaf288ac9fb720
Yule <13166835035@163.com>
2022-11-11 14:03:47 +0800

    html总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,31 +1,4 @@
-var romanToInt = function(s) {
-  let map = {
-      'I': 1,
-      'V': 5,
-      'x': 10,
-      'L': 50,
-      'c': 100,
-      'D': 500,
-      'M': 1000,
-      'IV': 4,
-      'IX': 9,
-      'XL': 40,
-      'XC': 90,
-      'CD': 400,
-      'CM': 900,
-  }
-  let res = 0;
-  for(let i = 0; i < s.length; i++) {
-      if (s[i] === 'I' && (s[i+1] === 'V' || s[i+1] === 'X') || 
-      s[i] === 'X' && (s[i+1] === 'L' || s[i+1] === 'C') ||
-      s[i] === 'C' && (s[i+1] === 'D' || s[i+1] === 'M')
-      ) {
-          res += map[s[i] + s[i + 1]];
-          i++
-      } else {
-          res += map[s[i]];
-      }
-  }
-  return res;
-};
-console.log(romanToInt("DCXXI"));
+onmessage = function(e) {
+  console.log(e);
+  postMessage([1,2,3])
+}
