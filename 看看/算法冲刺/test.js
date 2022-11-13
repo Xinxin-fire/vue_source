9672e75bd4989ca94be855d5735a22178caa3ae2
Xinxin-fire <13166835035@163.com>
2022-11-13 20:53:59 +0800

    html总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,4 +1,19 @@
-onmessage = function(e) {
-  console.log(e);
-  postMessage([1,2,3])
-}
+var reverseWords = function(s) {
+  let str = s.trim();
+  let arr = [];
+  let cur = ''
+  for (let i = 0; i <  str.length; i++) {
+      if (str[i] === ' ' && cur === '' && i !== 0) {
+          continue;
+      }
+      if (str[i] === ' ') {
+          arr.push(cur)
+          cur = ''
+      } else {
+
+        cur += str[i] 
+      }
+  }
+  return arr.reverse().join(' ')
+};
+console.log(reverseWords("the sky is blue"));
