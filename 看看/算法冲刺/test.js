d17c930326933901aff914093094755fa753f382
Xinxin-fire <13166835035@163.com>
2022-11-20 12:00:28 +0800

    css动画实现

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,16 +1,15 @@
-var longestPalindrome = function(s) {
-  let res = '';
-  function getRes(s, i, j) {
-      let left = i,right = j;
-      while(left >= 0 && right < s.length && s[left] === s[right]) {
-              left--;
-              right++;
-      }
-      return s.slice(left, right + 1)
-  }
-  for (let i = 0; i < s.length; i++) {
-      res = Math.max(getRes(s, i,i+1), getRes(s, i,i), res)
-  }
-  return res;
+
+var wordBreak = function(s, wordDict) {
+    if (s.length === 0) {
+        return true;
+    }
+    for (let item of wordDict) {
+        if (s.indexOf(item) === 0) {
+            if (wordBreak(s.slice(item.length), wordDict) === true) {
+              return true;
+            }
+        }
+    }
+    return false;
 };
-console.log(longestPalindrome('cbbd'));
+console.log(wordBreak("cars", ["car","ca","rs"]));
