787adff56d1d73d5334f0f000618a346d2c68ca5
Yule <13166835035@163.com>
2022-11-25 16:12:23 +0800

    js总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,15 +1,9 @@
-
-var wordBreak = function(s, wordDict) {
-    if (s.length === 0) {
-        return true;
-    }
-    for (let item of wordDict) {
-        if (s.indexOf(item) === 0) {
-            if (wordBreak(s.slice(item.length), wordDict) === true) {
-              return true;
-            }
-        }
-    }
-    return false;
-};
-console.log(wordBreak("cars", ["car","ca","rs"]));
+function a () {
+  var a = 1;
+  var b = 2;
+  return function b(){
+    console.log(a);
+  }
+}
+let c  = a();
+c()
