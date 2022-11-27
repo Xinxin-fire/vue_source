d92ebfd4adce37f2a8a9c6552f0895c903083c66
鑫鑫之火可以撩原 <13166835035@163.com>
2022-11-27 20:32:53 +0800

    javascript总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,9 +1,20 @@
-function a () {
-  var a = 1;
-  var b = 2;
-  return function b(){
-    console.log(a);
-  }
+let obj = {
+  a: 1,
+  b: 2
 }
-let c  = a();
-c()
+for (let item in obj) {
+  console.log(item)
+}
+
+function A (name){
+  this.name = name;
+}
+function B (name, age) {
+  B.prototype = new A();
+  this.age = age
+}
+B.prototype.a = '1';
+
+let d = new B('yuta', '18', '180')
+let c = new A('2')
+console.log(c.a)
