f39ac08a123e4a42942d834fac2fa324a11ea778
Yule <13166835035@163.com>
2022-12-03 11:13:11 +0800

    js总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,20 +1,6 @@
-let obj = {
-  a: 1,
-  b: 2
-}
-for (let item in obj) {
-  console.log(item)
-}
+console.log(1);
+import './test.css';
+document.onload = function() {
 
-function A (name){
-  this.name = name;
 }
-function B (name, age) {
-  B.prototype = new A();
-  this.age = age
-}
-B.prototype.a = '1';
-
-let d = new B('yuta', '18', '180')
-let c = new A('2')
-console.log(c.a)
+document.addEventListener('DOMContentLoaded')
