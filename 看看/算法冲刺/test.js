a3624ee0ad66b1f6f1a49ba0b9529260c0695dfd
鑫鑫之火可以撩原 <13166835035@163.com>
2023-01-02 20:59:39 +0800

    vue总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,72 +1,25 @@
-const data = [
-  {
-    value: 'w',
-    count: 5
-  },
-  {
-    value: 'e',
-    count: 4
-  },
-  {
-    value: 'v',
-    count: 2
-  },
-  {
-    value: 'g',
-    count: 2
-  },
-  {
-    value: 'a',
-    count: 2
-  },
-  {
-    value: 'e',
-    count: 2
-  },
-  {
-    value: '2',
-    count: 1
-  },
-  {
-    value: '9',
-    count: 1
-  },
-  {
-    value: '4',
-    count: 1
+let stack = [];
+// 最不活跃的就剔除；
+let arr = [1,2,3,4];
+stack.push(arr.pop())
+stack.push(arr.pop())
+stack.push(arr.pop())
+class LRU {
+  constructor(max) {
+    this.max = max;
+    this.instance = [];
   }
-]
-function sort(arr) {
-  arr.sort((a, b) =>  b.count - a.count);
-  for (let i = 0; i < arr.length; i++) {
-    if (!isNaN(arr[i].value)) {
-      arr.splice(i, 1);
-      i--
-      continue;
+  push(x) {
+    if(this.instance.length === max) {
+      this.instance.shift();
     }
-    let j = i + 1;
-    while(arr[j].count === arr[i].count && j < arr.length) {
-      j++;
-    }
-    if (j > i + 1) {
-      partSort(arr, i, j - 1);
-      i = j - 1;
+    this.instance.push(x);
+  }
+  get(x) {
+    if (this.instance.includes(x)) {
+      let index = this.instance.indexOf(x);
+      this.instance.splice(index, 1);
+      this.push(x);
     }
   }
-  function partSort(arr, i, j) {
-    console.log(i, j);
-    for (let m = i; m <=j; m++) {
-      for (let n = i + 1; n <=j; n++ ) {
-        if (arr[m].value.charCodeAt() > arr[n].value.charCodeAt()) {
-          let temp = arr[m];
-          arr[m] = arr[n];
-          arr[n] = temp;
-          // [arr[m], arr[n]] = [arr[n], arr[m]];
-        }
-      }
-    }
-  }
-  console.log(arr);
 }
-
-sort(data);
