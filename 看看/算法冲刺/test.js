24f82924bcb2348df28f7ad0d67064faea99d2dc
Yule <13166835035@163.com>
2023-01-13 17:18:12 +0800

    webpack修改

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,1 +1,20 @@
 
+class State {
+  static obj = {
+    a:1,
+    b: function() {
+      console.log(this);
+    }
+  }
+  getObj() {
+    console.log(this);
+  }
+}
+
+let state = new State();
+console.log(state);
+// state.obj.a = 2;
+state.obj.b();
+state.getObj();
+let state1 = new State();
+console.log(state1);
