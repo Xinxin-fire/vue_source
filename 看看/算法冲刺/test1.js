0560c761866ee01be78a1f6e8cece0363f607e3f
鑫鑫之火可以撩原 <13166835035@163.com>
2023-02-12 23:50:11 +0800

    项目课总结

--- 看看/算法冲刺/test1.js
+++ 看看/算法冲刺/test1.js
@@ -12,3 +12,13 @@
 // dfs(grid, i, j - 1);
 // // 标记已经递归过的路径
 // grid[i][j] = '0';
+
+// 买卖股票问题
+// 买卖一次
+// dp[i][0] 第i天不持有股票， dp[i][1]第i天持有股票
+// 买卖两次
+// 0未持有、1持有
+dp[i][2][0] = Math.max(dp[i-1][2][0], dp[i-1][2][1] - prices[i]);
+dp[i][2][1] = Math.max(dp[i-1][2][1], dp[i-1][1][0] + prices[i])
+dp[i][1][0] = Math.max(dp[i-1][1][0], dp[i-1][1][1] - prices[i]);
+dp[i][1][1] = Math.max(dp[i-1][1][1], dp[i-1][0][0] + prices[i])
