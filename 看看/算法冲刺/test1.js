4e37344db243316ce5b8a15b8c0572ce20c7853b
Yule <13166835035@163.com>
2023-02-14 17:34:47 +0800

    项目课总结

--- 看看/算法冲刺/test1.js
+++ 看看/算法冲刺/test1.js
@@ -1,24 +1,81 @@
 // // 岛屿题
-// // DFS递归
-// // 边界处理
-// if (i < 0 || j < 0 || i >= m || j >= n) {
-//   // 超出索引边界
-//   return;
-// }
-// // 递归方向
-// dfs(grid, i + 1, j);
-// dfs(grid, i, j + 1);
-// dfs(grid, i - 1, j);
-// dfs(grid, i, j - 1);
-// // 标记已经递归过的路径
-// grid[i][j] = '0';
+// DFS递归
+// 边界处理
+if (i < 0 || j < 0 || i >= m || j >= n) {
+  // 超出索引边界
+  return;
+}
+// 递归方向
+dfs(grid, i + 1, j);
+dfs(grid, i, j + 1);
+dfs(grid, i - 1, j);
+dfs(grid, i, j - 1);
+// 标记已经递归过的路径
+grid[i][j] = '0';
+var exist = function (board, word) {
+  let m = board.length;
+  let n = board[0].length;
+  let used = new Array(m).fill(false).map(() => new Array(n).fill(false))
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === word[0] && dfs(i, j, 0)) {
+          return true;
+      }
+    }
+  }
+  return false;
+  function dfs(i, j, curIndex) {
+    if (curIndex === word.length) {
+      return true;
+    }
+    if (i < 0 || i >= m || j < 0 || j >= m) {
+      return false;
+    }
+    if (used[i][j] || board[i][j] !== word[curIndex]) {
+      return false;
+    }
+    used[i][j] = true;
+    const flag = dfs(i + 1, j, curIndex + 1) || dfs(i - 1, j, curIndex + 1) || dfs(i, j + 1, curIndex + 1) || dfs(i, j -1, curIndex + 1)
+    used[i][j] = false;
+    return flag;
+  }
+};
+const exist = (board, word) => {
+  const m = board.length;
+  const n = board[0].length;
+  const used = new Array(m);    // 二维矩阵used，存放bool值
+  for (let i = 0; i < m; i++) {
+      used[i] = new Array(n);
+  }
+  for (let i = 0; i < m; i++) { // 遍历找起点，作为递归入口
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] == word[0] && canFind(i, j, 0)) { // 找到起点且递归结果为真，找到目标路径
+        return true; 
+      }
+    }
+  }
+  return false; // 怎么样都没有返回true，则返回false
+  function canFind(row, col, i) { // row col 当前点的坐标，i当前考察的word字符索引
+      if (i == word.length) {        // 递归的出口 i越界了就返回true
+          return true;
+      }
+      if (row < 0 || row >= m || col < 0 || col >= n) { // 当前点越界 返回false
+          return false;
+      }
+      if (used[row][col] || board[row][col] != word[i]) { // 当前点已经访问过，或，非目标点
+          return false;
+      }
+      // 排除掉所有false的情况，当前点暂时没毛病，可以继续递归考察
+      used[row][col] = true;  // 记录一下当前点被访问了
+      // canFindRest：基于当前选择的点[row,col]，能否找到剩余字符的路径。
+      const canFindRest = canFind(row + 1, col, i + 1) || canFind(row - 1, col, i + 1) ||
+          canFind(row, col + 1, i + 1) || canFind(row, col - 1, i + 1); 
 
-// 买卖股票问题
-// 买卖一次
-// dp[i][0] 第i天不持有股票， dp[i][1]第i天持有股票
-// 买卖两次
-// 0未持有、1持有
-dp[i][2][0] = Math.max(dp[i-1][2][0], dp[i-1][2][1] - prices[i]);
-dp[i][2][1] = Math.max(dp[i-1][2][1], dp[i-1][1][0] + prices[i])
-dp[i][1][0] = Math.max(dp[i-1][1][0], dp[i-1][1][1] - prices[i]);
-dp[i][1][1] = Math.max(dp[i-1][1][1], dp[i-1][0][0] + prices[i])
+      if (canFindRest) { // 基于当前点[row,col]，可以为剩下的字符找到路径
+          return true;    
+      }
+      used[row][col] = false; // 不能为剩下字符找到路径，返回false，撤销当前点的访问状态
+      return false;
+  };
+
+};
