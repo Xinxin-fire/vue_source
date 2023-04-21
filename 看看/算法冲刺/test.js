0e4d517bd7e9eb137379ae0644c2a64bceda2b50
Yule <13166835035@163.com>
2023-04-21 17:03:00 +0800

    算法总结

--- 看看/算法冲刺/test.js
+++ 看看/算法冲刺/test.js
@@ -1,7 +1,19 @@
-function getItemById(arr) {
-    arr.forEach((ele) => {
-      return ele
-    });
-  return 0;
-}
-console.log(getItemById([1]));
+var findUnsortedSubarray = function(nums) {
+  if (nums.length <= 1) {
+    return 0
+  }
+  let startIndex = endIndex =  0;
+  // let startVal = endVal =  nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[i-1]) {
+      if (!startIndex) {
+        startIndex = i-1;
+      }
+      endIndex = i;
+      [nums[i], nums[i-1]] = [nums[i - 1], nums[i]]
+    }
+  }
+  console.log(endIndex, startIndex)
+  return endIndex === startIndex ? 0 : endIndex - startIndex + 1
+};
+console.log(findUnsortedSubarray([5,4,3,2,1]));
